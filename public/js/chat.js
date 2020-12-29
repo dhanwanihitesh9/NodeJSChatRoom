08717b3b29495bf295e8e69c4bf859bb8dd32d38
Hitesh Dhanwani <dhanwanihitesh9@gmail.com>
2020-12-30 02:32:39 +0400

    scroll changes

--- public/js/chat.js
+++ public/js/chat.js
@@ -19,27 +19,29 @@ const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
 const {username, room} = Qs.parse(location.search, { ignoreQueryPrefix: true })
 
 const autoscroll = () => {
-    //New message element
-    const $newMessage = $messages.lastElementChild
-
-    //Height of new message
-    const newMessageStyles = getComputedStyle($newMessage)
-    const newMessageHeight = $newMessage.offsetHeight
-    const newMessageMargin = parseInt(newMessageStyles.marginBottom)
-    console.log(newMessageMargin)
-
-    //visible height
-    const visibleHeight = $messages.offsetHeight
-
-    //Height of messages container
-    const containerHeight = $messages.scrollHeight
-
-    //How far have i scrolled
-    const scrollOffset = $messages.scrollTop + visibleHeight
-
-    if(containerHeight - newMessageHeight <= scrollOffset) {
-        $messages.scrollTop = $messages.scrollHeight
+    $messages.scrollTop = $messages.scrollHeight
     }
+    //New message element
+    // const $newMessage = $messages.lastElementChild
+
+    // //Height of new message
+    // const newMessageStyles = getComputedStyle($newMessage)
+    // const newMessageHeight = $newMessage.offsetHeight
+    // const newMessageMargin = parseInt(newMessageStyles.marginBottom)
+    // console.log(newMessageMargin)
+
+    // //visible height
+    // const visibleHeight = $messages.offsetHeight
+
+    // //Height of messages container
+    // const containerHeight = $messages.scrollHeight
+
+    // //How far have i scrolled
+    // const scrollOffset = $messages.scrollTop + visibleHeight
+
+    // if(containerHeight - newMessageHeight <= scrollOffset) {
+    //     $messages.scrollTop = $messages.scrollHeight
+    // }
 }
 
 socket.on('message', (message) => {
